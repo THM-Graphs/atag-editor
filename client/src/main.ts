52a0c465c0254c6af5aff45e537f3b7325a258ca
Maximilian Michel <maximilian.michel@adwmainz.de>
2024-05-22 16:25:24 +0200

    Add empty text creation dialog

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,5 +5,6 @@ import router from './router.ts';
 import './style.css';
 import 'primevue/resources/themes/aura-light-green/theme.css';
 import 'primeicons/primeicons.css';
+import '/node_modules/primeflex/primeflex.css';
 
 createApp(App).use(router).use(PrimeVue).mount('#app');
--- client/src/views/Overview.vue
+++ client/src/views/Overview.vue
@@ -7,8 +7,10 @@ import InputText from 'primevue/inputtext';
 import IconField from 'primevue/iconfield';
 import InputIcon from 'primevue/inputicon';
 import Card from 'primevue/card';
+import Dialog from 'primevue/dialog';
 
 const collections = ref<ICollection[]>([]);
+const dialogIsVisible = ref<boolean>(false);
 
 onMounted(async (): Promise<void> => {
   collections.value = await getCollections();
@@ -48,8 +50,35 @@ async function getCollections(): Promise<ICollection[]> {
         </IconField>
       </template>
 
-      <template #end> <Button icon="pi pi-plus" aria-label="Submit" label="Add text" /> </template>
+      <template #end>
+        <Button
+          icon="pi pi-plus"
+          aria-label="Submit"
+          label="Add text"
+          @click="dialogIsVisible = true"
+        />
+      </template>
     </Toolbar>
+    <Dialog
+      v-model:visible="dialogIsVisible"
+      modal
+      header="Add new text"
+      :style="{ width: '25rem' }"
+    >
+      <div class="flex align-items-center gap-3 mb-3">
+        <label for="title" class="font-semibold w-6rem">Title</label>
+        <InputText id="title" class="flex-auto" autocomplete="off" />
+      </div>
+      <div class="flex justify-content-end gap-2">
+        <Button
+          type="button"
+          label="Cancel"
+          severity="secondary"
+          @click="dialogIsVisible = false"
+        ></Button>
+        <Button type="button" label="Create" @click="dialogIsVisible = false"></Button>
+      </div>
+    </Dialog>
 
     <span>{{ collections.length }} texts</span>
     <ul>
