ef8429eb00d22e5873a82c99a4f060734efd17fe
Maximilian Michel <maximilian.michel@adwmainz.de>
2024-05-29 16:00:31 +0200

    Make sidebars collapsible on button click

--- client/src/views/Editor.vue
+++ client/src/views/Editor.vue
@@ -16,10 +16,14 @@ const uuid: string = route.params.uuid as string;
 
 const collection = ref<ICollection | null>(null);
 const characters = ref<ICharacter[]>([]);
+const sidebars = ref([
+  { side: 'left', collapsed: false },
+  { side: 'right', collapsed: false },
+]);
 
 onMounted(async (): Promise<void> => {
-  await getCollectionByUuid(uuid);
-  await getCharacters(uuid);
+  await getCollectionByUuid();
+  await getCharacters();
 });
 
 // This allows using v-model for title input changes.
@@ -38,7 +42,7 @@ const displayedLabel: WritableComputedRef<string> = computed({
 // TODO: Replace with more generic async handling later
 const displayedUuid: ComputedRef<string> = computed(() => collection.value?.uuid || '');
 
-async function getCollectionByUuid(uuid: string): Promise<void> {
+async function getCollectionByUuid(): Promise<void> {
   try {
     // TODO: Replace localhost with vite configuration
     const url: string = `http://localhost:8080/api/collections/${uuid}`;
@@ -56,7 +60,30 @@ async function getCollectionByUuid(uuid: string): Promise<void> {
   }
 }
 
-async function getCharacters(uuid: string): Promise<void> {
+async function updateCollection(): Promise<void> {
+  try {
+    // TODO: Replace localhost with vite configuration
+    const url: string = `http://localhost:8080/api/collections/${uuid}`;
+    const response: Response = await fetch(url, {
+      method: 'POST',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify({ label: collection.value.label }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+  } catch (error: unknown) {
+    console.error('Error updating collection:', error);
+  }
+}
+
+async function getCharacters(): Promise<void> {
   try {
     // TODO: Replace localhost with vite configuration
     const url: string = `http://localhost:8080/api/collections/${uuid}/characters`;
@@ -76,11 +103,28 @@ async function getCharacters(uuid: string): Promise<void> {
     console.error('Error fetching characters:', error);
   }
 }
+
+function toggleSidebar(side: 'left' | 'right') {
+  const sidebar = sidebars.value.find(s => s.side === side);
+  sidebar.collapsed = sidebar.collapsed === true ? false : true;
+  console.log(sidebar);
+}
 </script>
 
 <template>
   <div class="container flex h-screen">
-    <section class="sidebar sidebar-left flex-1"></section>
+    <section
+      class="sidebar sidebar-left"
+      v-show="sidebars.find(s => s.side === 'left').collapsed === false"
+    ></section>
+    <div class="resizer resizer-left">
+      <Button
+        class="handle handle-left"
+        icon="pi pi-arrow-left"
+        severity="secondary"
+        @click="toggleSidebar('left')"
+      ></Button>
+    </div>
     <section class="main flex flex-column flex-grow-1">
       <div class="header">
         <div class="header-buttons flex">
@@ -111,17 +155,32 @@ async function getCharacters(uuid: string): Promise<void> {
         </div>
       </div>
       <div class="editor-button-container flex justify-content-center gap-3 p-3">
-        <Button aria-label="Save changes">Save</Button>
+        <Button aria-label="Save changes" @click="updateCollection">Save</Button>
         <Button severity="secondary" aria-label="Cancel changes">Cancel</Button>
       </div>
     </section>
-    <section class="sidebar sidebar-right flex-1"></section>
+    <div class="resizer resizer-right">
+      <Button
+        class="handle handle-right"
+        icon="pi pi-arrow-right"
+        severity="secondary"
+        @click="toggleSidebar('right')"
+      ></Button>
+    </div>
+    <section
+      class="sidebar sidebar-right"
+      v-show="sidebars.find(s => s.side === 'right').collapsed === false"
+    ></section>
   </div>
 </template>
 
 <style scoped>
 .main {
-  max-width: 80%;
+  flex-basis: 60%;
+}
+
+.sidebar {
+  flex-basis: 20%;
 }
 
 .content {
@@ -144,6 +203,50 @@ async function getCharacters(uuid: string): Promise<void> {
     outline: none;
     box-shadow: none;
     border-color: white;
+  }
+}
+
+.resizer {
+  width: 0.1rem;
+  cursor: col-resize;
+  background-color: black;
+  position: relative;
+
+  --height: 2.5rem;
+  --width: 1.25rem;
+
+  .handle {
+    /* display: flex; */
+    /* justify-content: center; */
+    /* align-items: center; */
+    /* background-color: #b2b2d7; */
+    /* color: #fff; */
+    /* border: 1px solid #b2b2d7; */
+    position: absolute;
+    height: var(--height);
+    width: var(--width);
+    top: 50%;
+    cursor: pointer;
+    /* z-index: 5; */
+    /* transition: background-color 100ms; */
+
+    &.handle-left {
+      left: 100%;
+      transform: translateY(-50%);
+      /* border-radius: 0 var(--width) var(--width) 0; */
+    }
+
+    &.handle-right {
+      right: 100%;
+      transform: translateY(-50%);
+      /* border-radius: var(--width) 0 0 var(--width); */
+    }
+
+    /* &:hover {
+      background-color: darkblue;
+      border: 1px solid darkblue;
+      transition: background-color 100ms;
+    } */
   }
 }
 </style>
