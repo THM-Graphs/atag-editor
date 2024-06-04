fd722c09cdc941dd31b82779b7c1924ebd82461f
Maximilian Michel <maximilian.michel@adwmainz.de>
2024-06-04 17:29:58 +0200

    Use guidelines for text creation dialog

--- client/src/components/OverviewToolbar.vue
+++ client/src/components/OverviewToolbar.vue
@@ -6,6 +6,8 @@ import InputText from 'primevue/inputtext';
 import IconField from 'primevue/iconfield';
 import InputIcon from 'primevue/inputicon';
 import Dialog from 'primevue/dialog';
+import Skeleton from 'primevue/skeleton';
+import { IGuidelines } from '../../../server/src/models/IGuidelines';
 
 const emit = defineEmits(['collectionCreated', 'searchInputChanged']);
 
@@ -13,6 +15,11 @@ const searchInput = ref<string>('');
 const dialogIsVisible = ref<boolean>(false);
 const newCollectionTitle = ref<string>('');
 
+const guidelines = ref<IGuidelines>({} as IGuidelines);
+
+const guidelinesAreLoaded = ref<boolean>(false);
+
+// TODO: Fix form submission
 async function createNewCollection(): Promise<void> {
   try {
     // TODO: Replace localhost with vite configuration
@@ -41,6 +48,33 @@ async function createNewCollection(): Promise<void> {
   }
 }
 
+async function showDialog(): Promise<void> {
+  dialogIsVisible.value = true;
+  await getGuidelines();
+}
+
+async function hideDialog(): Promise<void> {
+  dialogIsVisible.value = false;
+  guidelinesAreLoaded.value = false;
+}
+async function getGuidelines(): Promise<void> {
+  try {
+    // TODO: Replace localhost with vite configuration
+    const url: string = 'http://localhost:8080/api/guidelines';
+    const response: Response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const fetchedGuidelines: IGuidelines = await response.json();
+    guidelines.value = fetchedGuidelines;
+    guidelinesAreLoaded.value = true;
+  } catch (error: unknown) {
+    console.error('Error creating collection:', error);
+  }
+}
+
 function handleInput(): void {
   emit('searchInputChanged', searchInput.value.toLowerCase());
 }
@@ -58,38 +92,54 @@ function handleInput(): void {
     </template>
 
     <template #end>
-      <Button
-        icon="pi pi-plus"
-        aria-label="Submit"
-        label="Add text"
-        @click="dialogIsVisible = true"
-      />
+      <Button icon="pi pi-plus" aria-label="Submit" label="Add text" @click="showDialog" />
     </template>
   </Toolbar>
-  <Dialog v-model:visible="dialogIsVisible" modal header="Add new text" :style="{ width: '25rem' }">
+  <Dialog
+    v-model:visible="dialogIsVisible"
+    header="Add new text"
+    modal
+    :closable="false"
+    :close-on-escape="false"
+    :style="{ width: '25rem' }"
+  >
+    <template #header>
+      <h2 class="w-full text-center m-0">Add new text</h2>
+    </template>
     <form @submit.prevent="createNewCollection">
-      <div class="flex align-items-center gap-3 mb-3">
-        <label for="title" class="font-semibold w-6rem">Title</label>
-        <InputText
-          id="title"
-          class="flex-auto"
-          autocomplete="off"
-          required
-          autofocus
-          v-model="newCollectionTitle"
-        />
+      <div v-if="guidelinesAreLoaded" class="input-container">
+        <template v-for="(property, index) in guidelines.collections['text'].properties">
+          <div v-if="property.required" class="flex align-items-center gap-3 mb-3">
+            <label :for="property.name" class="font-semibold w-6rem">{{ property.name }} </label>
+            <InputText
+              :id="property.name"
+              :disabled="!property.editable"
+              :required="property.required"
+              :autofocus="index === 0"
+              :key="property.name"
+              class="flex-auto"
+              autocomplete="off"
+            />
+          </div>
+        </template>
       </div>
-      <div class="flex justify-content-end gap-2">
-        <Button
-          type="button"
-          label="Cancel"
-          severity="secondary"
-          @click="dialogIsVisible = false"
-        ></Button>
+      <div v-else class="skeleton-container">
+        <div v-for="n in 4" class="flex flex-row gap-2">
+          <Skeleton class="mb-3" height="2rem" width="10rem"></Skeleton>
+          <Skeleton class="mb-3" height="2rem"></Skeleton>
+        </div>
+      </div>
+
+      <div class="button-container flex justify-content-end gap-2">
+        <Button type="button" label="Cancel" severity="secondary" @click="hideDialog"></Button>
         <Button type="submit" label="Create"></Button>
       </div>
     </form>
   </Dialog>
 </template>
 
-<style scoped></style>
+<style scoped>
+label {
+  word-wrap: break-word;
+}
+</style>
