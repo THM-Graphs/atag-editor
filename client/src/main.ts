1d444b075a880dc736d48cd7578c24a6ea485ba9
Maximilian Michel <maximilian.michel@adwmainz.de>
2024-07-02 12:00:46 +0200

    Upgrade Primevue to v4

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,18 +1,26 @@
 import { createApp } from 'vue';
 import ConfirmationService from 'primevue/confirmationservice';
 import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import Tooltip from 'primevue/tooltip';
 import ToastService from 'primevue/toastservice';
 import App from './App.vue';
 import router from './router.ts';
 import './style.css';
-import Tooltip from 'primevue/tooltip';
-import 'primevue/resources/themes/aura-light-green/theme.css';
 import 'primeicons/primeicons.css';
 import '/node_modules/primeflex/primeflex.css';
 
 const app = createApp(App);
 
-app.use(router).use(PrimeVue).use(ConfirmationService).use(ToastService);
+app
+  .use(router)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+    },
+  })
+  .use(ConfirmationService)
+  .use(ToastService);
 
 app.directive('tooltip', Tooltip);
 
