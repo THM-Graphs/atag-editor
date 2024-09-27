42a86846450d9af9eb5601979ee34d5058fbf3b2
Maximilian Michel <maximilian.michel@adwmainz.de>
2024-09-27 15:27:38 +0200

    Fix image import errors

--- client/src/helper/icons.ts
+++ client/src/helper/icons.ts
@@ -1,8 +1,8 @@
+// TODO: This is not good, should be dynamic...
 import additionIcon from '../../public/icons/addition.svg';
 import emphasisedIcon from '../../public/icons/emphasised.svg';
 import expansionIcon from '../../public/icons/expansion.svg';
 import nonLinearIcon from '../../public/icons/nonLinear.svg';
-import marginNoteIcon from '../../public/icons/marginNote.svg';
 import interventionIcon from '../../public/icons/intervention.svg';
 import correctionIcon from '../../public/icons/correction.svg';
 import deletedIcon from '../../public/icons/deleted.svg';
@@ -10,18 +10,14 @@ import unclearIcon from '../../public/icons/unclear.svg';
 import repeatedIcon from '../../public/icons/repeated.svg';
 import gapIcon from '../../public/icons/gap.svg';
 import headIcon from '../../public/icons/head.svg';
-import pageIcon from '../../public/icons/page.svg';
-import columnIcon from '../../public/icons/column.svg';
 import lineIcon from '../../public/icons/line.svg';
 import transpositionIcon from '../../public/icons/transposition.svg';
-import initialIcon from '../../public/icons/initial.svg';
 
 const iconsMap: { [key: string]: string } = {
   addition: additionIcon,
   emphasised: emphasisedIcon,
   expansion: expansionIcon,
   nonLinear: nonLinearIcon,
-  marginNote: marginNoteIcon,
   intervention: interventionIcon,
   correction: correctionIcon,
   deleted: deletedIcon,
@@ -29,11 +25,8 @@ const iconsMap: { [key: string]: string } = {
   repeated: repeatedIcon,
   gap: gapIcon,
   head: headIcon,
-  page: pageIcon,
-  column: columnIcon,
   line: lineIcon,
   transposition: transpositionIcon,
-  initial: initialIcon,
 };
 
 export default iconsMap;
