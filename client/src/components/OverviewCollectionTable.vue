5a66008b38621c44a6fc18929b7081bb0541853f
Maximilian Michel <maximilian.michel@adwmainz.de>
2025-02-13 15:56:43 +0100

    Add text number to collection table

--- client/src/components/OverviewCollectionTable.vue
+++ client/src/components/OverviewCollectionTable.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { computed, ComputedRef, onMounted, ref } from 'vue';
 import { useGuidelinesStore } from '../store/guidelines';
 import LoadingSpinner from './LoadingSpinner.vue';
 import { buildFetchUrl, capitalize } from '../utils/helper/helper';
@@ -8,17 +8,30 @@ import DataTable from 'primevue/datatable';
 import { IGuidelines } from '../models/IGuidelines';
 import { CollectionAccessObject, CollectionProperty } from '../models/types';
 
-defineProps<{
+type CollectionTableEntry = {
+  [key: string | keyof CollectionProperty]: unknown;
+};
+
+const props = defineProps<{
   collections: CollectionAccessObject[] | null;
 }>();
 
 const { guidelines, getCollectionFields } = useGuidelinesStore();
-const columns = ref<CollectionProperty[]>([]);
+const columns = ref<string[]>([]);
+
+const tableData: ComputedRef<CollectionTableEntry[]> = computed(() => {
+  return props?.collections?.map(collection => {
+    return {
+      ...collection.collection.data,
+      texts: collection.texts.length,
+    };
+  });
+});
 
 onMounted(async (): Promise<void> => {
   await getGuidelines();
 
-  columns.value = getCollectionFields();
+  columns.value = [...getCollectionFields().map(f => f.name), 'texts'];
 });
 
 // TODO: getGuidelines exists in multiple components now, should be moved to a shared location
@@ -43,13 +56,14 @@ async function getGuidelines(): Promise<void> {
 
 <template>
   <div class="flex-grow-1 overflow-y-auto">
-    <LoadingSpinner v-if="!collections" />
+    <LoadingSpinner v-if="!tableData" />
     <!-- TODO: Fix sorting... -->
     <DataTable
       v-else
       scrollable
+      :rowClass="() => 'bitch'"
       scrollHeight="flex"
-      :value="collections"
+      :value="tableData"
       paginator
       :rows="10"
       :rowsPerPageOptions="[10, 20, 50, 100]"
@@ -63,26 +77,29 @@ async function getGuidelines(): Promise<void> {
     >
       <Column
         v-for="col of columns"
-        :key="col.name"
-        :field="col.name"
-        :header="capitalize(col.name)"
+        :key="col"
+        :field="col"
+        :header="capitalize(col)"
         sortable
+        :headerStyle="col === 'texts' ? `width: 5rem` : ''"
       >
         <template #body="{ data }">
           <!-- TODO: This should come from the configuration... -->
+          <!-- TODO: Bit hacky. Beautify? -->
           <a
-            v-if="col.name === 'label'"
+            v-if="col === 'label'"
             class="cell-link"
-            :href="'/collections/' + data.collection.data.uuid"
-            v-tooltip.hover.top="{ value: data.collection.data[col.name], showDelay: 0 }"
-            >{{ data.collection.data[col.name] }}</a
+            :href="'/collections/' + data.uuid"
+            v-tooltip.hover.top="{ value: data[col], showDelay: 0 }"
+            >{{ data[col] }}</a
           >
           <span
-            v-else
+            v-else-if="col !== 'texts'"
             class="cell-info"
-            v-tooltip.hover.top="{ value: data.collection.data[col.name], showDelay: 0 }"
-            >{{ data.collection.data[col.name] }}</span
+            v-tooltip.hover.top="{ value: data[col], showDelay: 0 }"
+            >{{ data[col] }}</span
           >
+          <span v-else class="cell-info w-2rem">{{ data.texts }}</span>
         </template>
       </Column>
     </DataTable>
