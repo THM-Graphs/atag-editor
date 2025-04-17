632c8675837cd49c1151097235ed247cd8940cb1
Maximilian Michel <maximilian.michel@adwmainz.de>
2025-04-17 12:09:26 +0200

    Use conversion helpers for annotations (backend)

--- server/src/services/guidelines.service.ts
+++ server/src/services/guidelines.service.ts
@@ -4,8 +4,66 @@ import NotFoundError from '../errors/not-found.error.js';
 
 export default class GuidelinesService {
   /**
-   * Retrieves field configuration of a collection with given additional node labels. Contains information about validation rules (required/not required).
+   * Retrieves the field configuration of an annotation with the given type.
+   *
+   * The method operates on the given guidelines parameter instead of fetching from within. This is done
+   * to prevent multiple requests, since the method is used to preprocess a batch of annotations before
+   * saving them.
+   *
+   * @param {IGuidelines} guidelines - The guidelines to retrieve the field configuration from.
+   * @param {string} type - The type of the annotation.
+   * @return {PropertyConfig[]} The field configuration for the annotation type.
+   */
+  public getAnnotationConfigFieldsFromGuidelines(
+    guidelines: IGuidelines,
+    type: string,
+  ): PropertyConfig[] {
+    // TODO: This is preliminary, since annotations do not have the system/base/additional categories yet
+    const system: PropertyConfig[] = [];
+    const base: PropertyConfig[] = guidelines.annotations.properties;
+    const additional: PropertyConfig[] =
+      guidelines.annotations.types.find(annoConfig => annoConfig.type === type)?.properties ?? [];
+
+    return [...system, ...base, ...additional];
+  }
+
+  /**
+   * Retrieves the field configuration of a collection with the given additional node labels.
+   *
+   * The method operates on the given guidelines parameter instead of fetching from within. This is done
+   * to prevent multiple requests, since the method is used to preprocess a batch of collections before
+   * saving them.
+   *
+   * @param {IGuidelines} guidelines - The guidelines to retrieve the field configuration from.
+   * @param {string[]} nodeLabels - The additional labels of the collection.
+   * @return {PropertyConfig[]} The field configuration for the collection type.
+   */
+  public getCollectionConfigFieldsFromGuidelines(
+    guidelines: IGuidelines,
+    nodeLabels: string[],
+  ): PropertyConfig[] {
+    const system: PropertyConfig[] = guidelines.collections.properties.system;
+    const base: PropertyConfig[] = guidelines.collections.properties.base;
+    const additional: PropertyConfig[] = guidelines.collections.types.reduce(
+      (total: PropertyConfig[], curr) => {
+        if (nodeLabels.includes(curr.additionalLabel)) {
+          total.push(...curr.properties);
+        }
+        return total;
+      },
+      [],
+    );
+
+    return [...system, ...base, ...additional];
+  }
+
+  /**
+   * Retrieves field configuration of a collection with given additional node labels.
+   * Contains information about validation rules (required/not required).
    * Used for applying default data to to-be-created collections when they are missing
+   *
+   * The method fetches the guidelines from the guidelines URL defined in the GUIDELINES_URL environment variable
+   * since it will not be called multiple times in the same context (like `getCollectionConfigFieldsFromGuidelines` or `getAnnotationConfigFieldsFromGuidelines`)
    *
    * @param {string[]} nodeLabels - The additional labels of the collection.
    * @return {Promise<PropertyConfig[]>} The field configurations for the collection type.
