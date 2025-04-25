2a3a6d5099a7b570f10c5ba24fac5aae91b47c7a
Maximilian Michel <maximilian.michel@adwmainz.de>
2025-04-25 12:51:26 +0200

    Remove hardcoded entries from IAnnotation interface

--- server/src/models/IAnnotation.ts
+++ server/src/models/IAnnotation.ts
@@ -1,13 +1,9 @@
 export default interface IAnnotation {
   [keyof: string]: any;
-  comment: string;
-  commentInternal: string;
   endIndex: number;
-  originalText: string;
   startIndex: number;
   subtype: string | number;
   text: string;
   type: string;
-  url: string;
   uuid: string;
 }
