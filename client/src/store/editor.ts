7f624a177690c641c1c299059510ca5feaac809f
Maximilian Michel <maximilian.michel@adwmainz.de>
2025-05-28 16:33:16 +0200

    Format yml files

--- client/src/store/editor.ts
+++ client/src/store/editor.ts
@@ -229,7 +229,7 @@ export function useEditorStore() {
     }
 
     snippetCharacters.value = cloneDeep(newLastRecord.data.characters);
-    annotations.value = cloneDeep(newLastRecord.data.annotations);
+    // annotations.value = cloneDeep(newLastRecord.data.annotations);
     setBeforeStartCharacter(cloneDeep(newLastRecord.data.beforeStartCharacter));
     setAfterEndCharacter(cloneDeep(newLastRecord.data.afterEndCharacter));
 
@@ -306,7 +306,7 @@ export function useEditorStore() {
     // }
 
     snippetCharacters.value = cloneDeep(record.data.characters);
-    annotations.value = cloneDeep(record.data.annotations);
+    // annotations.value = cloneDeep(record.data.annotations);
     setBeforeStartCharacter(cloneDeep(record.data.beforeStartCharacter));
     setAfterEndCharacter(cloneDeep(record.data.afterEndCharacter));
 
