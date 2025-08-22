b54b449b2a24b66ab2f61874199b775ac3d58507
Maximilian Michel <maximilian.michel@adwmainz.de>
2025-08-22 10:36:35 +0200

    Show data in sidebar, update breadcrumbs

--- client/src/views/CollectionManager.vue
+++ client/src/views/CollectionManager.vue
@@ -220,72 +220,92 @@ function showMessage(operation: 'created' | 'deleted', detail?: string): void {
 </script>
 
 <template>
-  <div class="container flex flex-column h-screen m-auto">
-    <Toast />
+  <Toast />
 
-    <h2 class="text-center text-5xl line-height-2">
-      Collection Manager for {{ route.params.uuid ? collection?.data.label : '' }}
-    </h2>
+  <h2 class="text-center text-5xl line-height-2">
+    Collection Manager for {{ route.params.uuid ? collection?.data.label : '' }}
+  </h2>
+  <div class="flex">
+    <div class="container flex flex-column h-screen m-auto">
+      <div class="breadcrumbs-pane">
+        <div v-for="path in ancestryPaths">
+          <span>
+            <RouterLink to="/collection-manager" :title="`Go to Collection Manager Overview`">
+              <i class="pi pi-home"></i>
+            </RouterLink>
+          </span>
+          <span v-for="node in path">
+            <span> -> </span>
+            <RouterLink
+              v-if="node.nodeLabels.includes('Collection')"
+              :to="`/collection-manager/${node.data.uuid}`"
+              :title="`Go to Collection ${node.data.uuid}`"
+            >
+              {{ node.data.label }}
+              <i class="pi pi-external-link"></i>
+            </RouterLink>
+            <a
+              v-else-if="node.nodeLabels.includes('Text')"
+              :href="`/texts/${node.data.uuid}`"
+              :title="`Go to Text ${node.data.uuid}`"
+              target="_blank"
+            >
+              Text
+              <i class="pi pi-external-link"></i>
+            </a>
+            <span v-else> Annotation {{ node.data.type }}</span>
+          </span>
+          <span>
+            <span>-></span>
+            <span class="font-bold font-italic">{{ collection.data.label }}</span>
+          </span>
+        </div>
+      </div>
 
-    <div class="breadcrumbs-pane">
-      <div v-for="path in ancestryPaths">
-        <span>
-          <RouterLink to="/collection-manager" :title="`Go to Collection Manager Overview`">
-            <i class="pi pi-home"></i>
-          </RouterLink>
-        </span>
-        <span v-for="node in path">
-          <span> -> </span>
-          <RouterLink
-            v-if="node.nodeLabels.includes('Collection')"
-            :to="`/collection-manager/${node.data.uuid}`"
-            :title="`Go to Collection ${node.data.uuid}`"
-          >
-            {{ node.data.label }}
-            <i class="pi pi-external-link"></i>
-          </RouterLink>
-          <a
-            v-else-if="node.nodeLabels.includes('Text')"
-            :href="`/texts/${node.data.uuid}`"
-            :title="`Go to Text ${node.data.uuid}`"
-            target="_blank"
-          >
-            Text
-            <i class="pi pi-external-link"></i>
-          </a>
-          <span v-else> Annotation {{ node.data.type }}</span>
-        </span>
+      <OverviewToolbar
+        v-if="guidelines"
+        :searchInputValue="searchParams.searchInput"
+        :nodeLabelsValue="searchParams.nodeLabels as string[]"
+        @collection-created="handleCollectionCreation"
+        @search-input-changed="handleSearchInputChange"
+        @node-labels-input-changed="handleNodeLabelsInputChanged"
+      />
+
+      <div class="counter text-right pt-2 pb-3">
+        <strong class="text-base"
+          >{{ pagination ? pagination.totalRecords : 0 }} Collections in total</strong
+        >
+      </div>
+
+      <CollectionTable
+        v-if="guidelines"
+        @sort-changed="handleSortChange"
+        @pagination-changed="handlePaginationChange"
+        :collections="collections"
+        :pagination="pagination"
+        :async-operation-running="asyncOperationRunning"
+        mode="edit"
+      />
+    </div>
+
+    <div class="collection-data text-center">
+      <div>Data of current collection</div>
+      <br />
+      <div>
+        {{ collection?.nodeLabels }}
+      </div>
+      <br />
+      <div>
+        {{ collection?.data }}
       </div>
     </div>
-
-    <OverviewToolbar
-      v-if="guidelines"
-      :searchInputValue="searchParams.searchInput"
-      :nodeLabelsValue="searchParams.nodeLabels as string[]"
-      @collection-created="handleCollectionCreation"
-      @search-input-changed="handleSearchInputChange"
-      @node-labels-input-changed="handleNodeLabelsInputChanged"
-    />
-
-    <div class="counter text-right pt-2 pb-3">
-      <strong class="text-base"
-        >{{ pagination ? pagination.totalRecords : 0 }} Collections in total</strong
-      >
-    </div>
-
-    <CollectionTable
-      v-if="guidelines"
-      @sort-changed="handleSortChange"
-      @pagination-changed="handlePaginationChange"
-      :collections="collections"
-      :pagination="pagination"
-      :async-operation-running="asyncOperationRunning"
-      mode="edit"
-    />
   </div>
 </template>
 
 <style scoped>
+.collection-data {
+  width: 250px;
+}
 .container {
   width: 80%;
   min-width: 800px;
