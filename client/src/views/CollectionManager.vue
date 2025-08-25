27c1ef3abfd5ca73e37b9911e7b0cec34f6fb8fe
Maximilian Michel <maximilian.michel@adwmainz.de>
2025-08-25 14:10:40 +0200

    Implement error page on collection manager page

--- client/src/views/CollectionManager.vue
+++ client/src/views/CollectionManager.vue
@@ -5,6 +5,8 @@ import { useTitle } from '@vueuse/core';
 import { useGuidelinesStore } from '../store/guidelines';
 import { useCollectionSearch } from '../composables/useCollectionSearch';
 import CollectionCreationButton from '../components/CollectionCreationButton.vue';
+import CollectionError from '../components/CollectionError.vue';
+import LoadingSpinner from '../components/LoadingSpinner.vue';
 import OverviewToolbar from '../components/OverviewToolbar.vue';
 import CollectionTable from '../components/CollectionTable.vue';
 import Button from 'primevue/button';
@@ -46,6 +48,7 @@ const ancestryPaths = ref<NodeAncestry[] | null>(null);
 
 // Initial pageload
 const isLoading = ref<boolean>(false);
+const isValidCollection = ref<boolean>(false);
 // For other async operations
 const asyncOperationRunning = ref<boolean>(false);
 
@@ -54,24 +57,29 @@ watch(
   async (newUuid: string) => {
     isLoading.value = true;
 
-    await getGuidelines();
+    if (newUuid) {
+      await getCollection();
+    }
 
     // Reset ancestry - If no uuid param is set, needs to be cleanded anyway
     ancestryPaths.value = [];
 
     // Fetch parent collection details and ancestry only if a UUID is present
-    if (newUuid) {
-      await getCollection();
+    if (isValidCollection.value) {
       await getCollectionAncestry();
     }
 
-    // Collection filter params need to be reset
-    resetCollectionSearchParams();
+    if (isValidCollection.value || newUuid === '') {
+      await getGuidelines();
 
-    // Explicitly update the composable's uuid
-    updateUuid(newUuid || undefined);
+      // Collection filter params need to be reset
+      resetCollectionSearchParams();
 
-    // Collections are fetched via watcher
+      // Explicitly update the composable's uuid
+      updateUuid(newUuid || undefined);
+
+      // Collections are fetched via watcher
+    }
 
     isLoading.value = false;
   },
@@ -95,6 +103,7 @@ async function getCollection(): Promise<void> {
 
     const fetchedCollection: Collection = await response.json();
 
+    isValidCollection.value = true;
     collection.value = fetchedCollection;
   } catch (error: unknown) {
     console.error('Error fetching collection:', error);
@@ -212,117 +221,125 @@ function showMessage(operation: 'created' | 'deleted', detail?: string): void {
 <template>
   <Toast />
 
-  <div class="header-buttons flex justify-content-between mx-2 pl-2 pt-2">
-    <RouterLink to="/">
-      <Button
-        icon="pi pi-home"
-        aria-label="Home"
-        class="w-2rem h-2rem"
-        title="Go to overview"
-      ></Button>
-    </RouterLink>
-    <div class="flex">
-      <RouterLink :to="`/collections/${collection?.data.uuid}`">
+  <LoadingSpinner v-if="isLoading === true" />
+  <CollectionError
+    v-else-if="isValidCollection === false && route.params.uuid !== ''"
+    :uuid="route.params.uuid as string"
+  />
+
+  <template v-else>
+    <div class="header-buttons flex justify-content-between mx-2 pl-2 pt-2">
+      <RouterLink to="/">
         <Button
-          icon="pi pi-pen-to-square"
-          label="Edit collection data"
-          severity="secondary"
-          aria-label="Open this collection in Collection editor"
-          title="Open this collection in Collection editor"
+          icon="pi pi-home"
+          aria-label="Home"
+          class="w-2rem h-2rem"
+          title="Go to overview"
         ></Button>
       </RouterLink>
-    </div>
-  </div>
-  <h2 class="text-center text-5xl line-height-2">
-    Collection Manager for {{ route.params.uuid ? collection?.data.label : '' }}
-  </h2>
-  <div class="flex">
-    <div class="container flex flex-column h-screen m-auto">
-      <div class="breadcrumbs-pane">
-        <div v-for="path in ancestryPaths">
-          <span>
-            <RouterLink to="/collection-manager" :title="`Go to Collection Manager Overview`">
-              <i class="pi pi-home"></i>
-            </RouterLink>
-          </span>
-          <span v-for="node in path">
-            <span> -> </span>
-            <RouterLink
-              v-if="node.nodeLabels.includes('Collection')"
-              :to="`/collection-manager/${node.data.uuid}`"
-              :title="`Go to Collection ${node.data.uuid}`"
-            >
-              {{ node.data.label }}
-              <i class="pi pi-external-link"></i>
-            </RouterLink>
-            <a
-              v-else-if="node.nodeLabels.includes('Text')"
-              :href="`/texts/${node.data.uuid}`"
-              :title="`Go to Text ${node.data.uuid}`"
-              target="_blank"
-            >
-              Text
-              <i class="pi pi-external-link"></i>
-            </a>
-            <span v-else> Annotation {{ node.data.type }}</span>
-          </span>
-          <span>
-            <span>-></span>
-            <span class="font-bold font-italic">{{ collection.data.label }}</span>
-          </span>
-        </div>
+      <div class="flex">
+        <RouterLink :to="`/collections/${collection?.data.uuid}`">
+          <Button
+            icon="pi pi-pen-to-square"
+            label="Edit collection data"
+            severity="secondary"
+            aria-label="Open this collection in Collection editor"
+            title="Open this collection in Collection editor"
+          ></Button>
+        </RouterLink>
       </div>
+    </div>
+    <h2 class="text-center text-5xl line-height-2">
+      Collection Manager for {{ route.params.uuid ? collection?.data.label : '' }}
+    </h2>
+    <div class="flex">
+      <div class="container flex flex-column h-screen m-auto">
+        <div class="breadcrumbs-pane">
+          <div v-for="path in ancestryPaths">
+            <span>
+              <RouterLink to="/collection-manager" :title="`Go to Collection Manager Overview`">
+                <i class="pi pi-home"></i>
+              </RouterLink>
+            </span>
+            <span v-for="node in path">
+              <span> -> </span>
+              <RouterLink
+                v-if="node.nodeLabels.includes('Collection')"
+                :to="`/collection-manager/${node.data.uuid}`"
+                :title="`Go to Collection ${node.data.uuid}`"
+              >
+                {{ node.data.label }}
+                <i class="pi pi-external-link"></i>
+              </RouterLink>
+              <a
+                v-else-if="node.nodeLabels.includes('Text')"
+                :href="`/texts/${node.data.uuid}`"
+                :title="`Go to Text ${node.data.uuid}`"
+                target="_blank"
+              >
+                Text
+                <i class="pi pi-external-link"></i>
+              </a>
+              <span v-else> Annotation {{ node.data.type }}</span>
+            </span>
+            <span>
+              <span>-></span>
+              <span class="font-bold font-italic">{{ collection.data.label }}</span>
+            </span>
+          </div>
+        </div>
 
-      <div class="flex gap-2">
-        <OverviewToolbar
+        <div class="flex gap-2">
+          <OverviewToolbar
+            v-if="guidelines"
+            :searchInputValue="searchParams.searchInput"
+            :nodeLabelsValue="searchParams.nodeLabels as string[]"
+            @search-input-changed="handleSearchInputChange"
+            @node-labels-input-changed="handleNodeLabelsInputChanged"
+          />
+
+          <CollectionCreationButton
+            :parent-collection="collection"
+            @collection-created="handleCollectionCreation"
+          />
+        </div>
+
+        <div class="counter text-right pt-2 pb-3">
+          <strong class="text-base"
+            >{{ pagination ? pagination.totalRecords : 0 }} Collections in total</strong
+          >
+        </div>
+
+        <CollectionTable
           v-if="guidelines"
-          :searchInputValue="searchParams.searchInput"
-          :nodeLabelsValue="searchParams.nodeLabels as string[]"
-          @search-input-changed="handleSearchInputChange"
-          @node-labels-input-changed="handleNodeLabelsInputChanged"
-        />
-
-        <CollectionCreationButton
-          :parent-collection="collection"
-          @collection-created="handleCollectionCreation"
+          @sort-changed="handleSortChange"
+          @pagination-changed="handlePaginationChange"
+          :collections="collections"
+          :pagination="pagination"
+          :async-operation-running="asyncOperationRunning"
+          mode="edit"
         />
       </div>
 
-      <div class="counter text-right pt-2 pb-3">
-        <strong class="text-base"
-          >{{ pagination ? pagination.totalRecords : 0 }} Collections in total</strong
-        >
-      </div>
-
-      <CollectionTable
-        v-if="guidelines"
-        @sort-changed="handleSortChange"
-        @pagination-changed="handlePaginationChange"
-        :collections="collections"
-        :pagination="pagination"
-        :async-operation-running="asyncOperationRunning"
-        mode="edit"
-      />
-    </div>
-
-    <div class="collection-data text-center">
-      <div>Data of current collection</div>
-      <br />
-
-      <template v-if="collection">
-        <div>
-          {{ collection?.nodeLabels }}
-        </div>
+      <div class="collection-data text-center">
+        <div>Data of current collection</div>
         <br />
-        <div>
-          {{ collection?.data }}
-        </div>
-      </template>
-      <template v-else>
-        <span class="font-italic"> No collection selected </span>
-      </template>
+
+        <template v-if="collection">
+          <div>
+            {{ collection?.nodeLabels }}
+          </div>
+          <br />
+          <div>
+            {{ collection?.data }}
+          </div>
+        </template>
+        <template v-else>
+          <span class="font-italic"> No collection selected </span>
+        </template>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <style scoped>
