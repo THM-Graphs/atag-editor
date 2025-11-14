e3f23de34bb7a9a1d30578ed4f424d689e622825
Maximilian Michel <maximilian.michel@adwmainz.de>
2025-11-14 14:28:28 +0100

    Make column items look better

--- client/src/components/CollectionItem.vue
+++ client/src/components/CollectionItem.vue
@@ -19,10 +19,21 @@ function handleItemClick(): void {
   <div
     class="container p-1"
     :class="{ active: props.isActive, temporary: props.collection.status === 'temporary' }"
+    :title="`Click to show details of ${props.collection.data.data.label}`"
     @click="handleItemClick"
   >
     <div class="labels">
       <NodeTag
+        :style="{
+          fontSize: '0.7rem',
+          backgroundColor: 'white',
+          fontWeight: 'normal',
+          color: 'black',
+          padding: '2px 2px',
+          lineHeight: '100%',
+          border: '1px solid black',
+        }"
+        class="test mr-1"
         v-for="label in props.collection.data.nodeLabels"
         :content="label"
         type="Collection"
@@ -41,6 +52,14 @@ function handleItemClick(): void {
 
   &:hover {
     background-color: hsl(0, 0%, 90%);
+  }
+
+  .label {
+    height: 1.5rem;
+    font-size: 0.9rem;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
   }
 
   .active {
