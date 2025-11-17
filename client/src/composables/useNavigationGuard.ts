d43cb61d9dfdd27be841d3d1f894229b8e4cb587
Maximilian Michel <maximilian.michel@adwmainz.de>
2025-11-17 15:17:40 +0100

    Redirect uuid-targeted links of colloections to their position in the hierarchy

--- client/src/composables/useNavigationGuard.ts
+++ client/src/composables/useNavigationGuard.ts
@@ -1,7 +1,9 @@
+import { RouteLocationNormalized, RouteLocationRaw } from 'vue-router';
+import { NodeAncestry } from '../models/types';
 import { useAppStore } from '../store/app';
 
 export function useNavigationGuard() {
-  const { activeModal } = useAppStore();
+  const { activeModal, api } = useAppStore();
 
   function hasOpenModal(): boolean {
     if (!activeModal.value) {
@@ -18,5 +20,41 @@ export function useNavigationGuard() {
     }
   }
 
-  return { hasOpenModal };
+  /**
+   * Redirects to the column view path with the collection ancestry as a query parameter.
+   *
+   * When navigating to a specific collection with given UUID, this function fetches the ancestry of the item
+   * and redirects to the path with the collection ancestry as a query parameter.
+   *
+   * Created to redirect links that point to a location (`/collections/:uuid`) to their representation
+   * in the column view (`?path=uuid1,uuid2,uuid3,...`).
+   *
+   * @param {RouteLocationNormalized} route - The route object containing the route parameters.
+   * @returns {Promise<RouteLocationRaw>} - A promise that resolves with the redirect location.
+   */
+  async function redirectToCollectionPath(
+    route: RouteLocationNormalized,
+  ): Promise<RouteLocationRaw> {
+    const uuid: string = route.params.uuid as string;
+    const ancestryPaths: NodeAncestry[] = await api.getCollectionAncestry(uuid);
+    const desired: NodeAncestry | null = ancestryPaths[0] ?? null;
+
+    let pathQuery: string = '';
+
+    // The api response only contains item's the ancestry, not the item itself
+    if (desired?.length > 0) {
+      pathQuery = [...desired.map(n => n.data.uuid), uuid].join(',');
+    } else {
+      pathQuery = uuid;
+    }
+
+    return {
+      path: '/',
+      query: {
+        path: pathQuery,
+      },
+    };
+  }
+
+  return { hasOpenModal, redirectToCollectionPath };
 }
--- client/src/router.ts
+++ client/src/router.ts
@@ -3,20 +3,26 @@ import Editor from './views/Editor.vue';
 import CollectionManager from './views/CollectionManager.vue';
 import { useNavigationGuard } from './composables/useNavigationGuard';
 
-const { hasOpenModal } = useNavigationGuard();
+const { hasOpenModal, redirectToCollectionPath } = useNavigationGuard();
 
 const allRoutes = [
   { path: '/', component: CollectionManager },
+  { path: '/test', component: CollectionManager },
+  {
+    path: '/collections/:uuid',
+    component: { template: '</div>' },
+    beforeEnter: redirectToCollectionPath,
+  },
   { path: '/texts/:uuid', component: Editor },
 ];
 
 const prodRoutes = allRoutes.filter(r => r.path !== '/playground');
 
-const usedRoutes: RouteRecordRaw[] = import.meta.env.DEV ? allRoutes : prodRoutes;
+const usedRoutes = import.meta.env.DEV ? allRoutes : prodRoutes;
 
 const router: Router = createRouter({
   history: createWebHistory(),
-  routes: usedRoutes,
+  routes: usedRoutes as RouteRecordRaw[],
 });
 
 router.beforeEach(() => hasOpenModal());
