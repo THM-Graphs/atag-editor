9dba030bc579a040a156ab3beaef75214293e6b7
Maximilian Michel <maximilian.michel@adwmainz.de>
2026-04-07 12:49:35 +0200

    Add basic annotation form pane (wip)

--- client/src/views/Playground.vue
+++ client/src/views/Playground.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { EditorContent, Extension } from '@tiptap/vue-3';
-import { onMounted, onUnmounted } from 'vue';
+import { onMounted, onUnmounted, provide } from 'vue';
 
 import { useEditorStore } from '../store/editor';
+import EditorAnnotationButtonPaneNew from '../components/EditorAnnotationButtonPaneNew.vue';
+import { Annotation } from '../models/types';
+import { Card } from 'primevue';
+import AnnotationTypeIcon from '../components/AnnotationTypeIcon.vue';
 
-const { tiptap, initializeTiptap, destroyTiptap } = useEditorStore();
+const { tiptap, initializeTiptap, destroyTiptap, tiptapAnnotations } = useEditorStore();
 
 onMounted(() => initializeTiptap());
 onUnmounted(() => destroyTiptap());
@@ -12,6 +16,22 @@ onUnmounted(() => destroyTiptap());
 function handleClick() {
   tiptap.value.state.doc.descendants((node, pos) => {
     console.log(pos, node);
+  });
+}
+
+function toggleTextHightlighting(annotation: Annotation, direction: 'on' | 'off'): void {
+  const annotatedSpans: NodeListOf<HTMLSpanElement> = document.querySelectorAll(
+    `#editor span[data-anno-uuid="${annotation.data.properties.uuid}"]`,
+  );
+
+  if (annotatedSpans.length === 0) {
+    return;
+  }
+
+  // scrollIntoViewIfNeeded(annotatedSpans[0]);
+
+  annotatedSpans.forEach((span: HTMLSpanElement) => {
+    direction === 'on' ? span.classList.add('highlight') : span.classList.remove('highlight');
   });
 }
 </script>
@@ -82,17 +102,60 @@ function handleClick() {
       >
         lineheight DOWN
       </button>
+
       <button
         @click="
-          tiptap?.chain().focus().setMark('bold', { class: 'bold-tag' }).setTextSelection(1).run()
+          tiptap
+            ?.chain()
+            .focus()
+            .setZeroPointAnnotation({
+              type: 'deleted',
+              subType: '',
+              isZeroPoint: true,
+              uuid: '123',
+            })
+            .run()
         "
-        :class="{ 'is-active': tiptap?.isActive('link') }"
+        :class="{ 'is-active': tiptap?.isActive('zeroPointAnnotation') }"
       >
-        extendmarkrange
+        Zero point test
       </button>
     </div>
+    <EditorAnnotationButtonPaneNew />
+    <div class="flex">
+      <editor-content id="editor" :editor="tiptap" />
+      <div>
+        <h3>Annotations</h3>
 
-    <editor-content id="editor" :editor="tiptap" />
+        <Card v-for="annotation in tiptapAnnotations" :key="annotation.data.properties.uuid">
+          <template #title>
+            <div class="flex items-center gap-1 align-items-center">
+              <div class="icon-container" style="width: 20px; height: 20px">
+                <AnnotationTypeIcon
+                  :annotationType="
+                    annotation.data.properties.subType ?? annotation.data.properties.type
+                  "
+                />
+              </div>
+              <div class="annotation-type-container">
+                <span class="font-bold">{{
+                  annotation.data.properties.subType ?? annotation.data.properties.type
+                }}</span>
+              </div>
+              <div
+                class="spy pi pi-eye cursor-pointer"
+                title="Show annotated text"
+                @mouseover="toggleTextHightlighting(annotation, 'on')"
+                @mouseleave="toggleTextHightlighting(annotation, 'off')"
+              ></div>
+            </div>
+          </template>
+          <template #content>
+            <p class="m-0"></p>
+          </template>
+        </Card>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -105,8 +168,12 @@ function handleClick() {
     outline: 0;
   }
 
+  span.highlight {
+    background-color: yellow !important;
+  }
+
   /* Unique data id */
-  [data-anno-uuid] {
+  /* [data-anno-uuid] {
     border: 2px solid black;
     border-radius: 0.5rem;
     padding: 2.5rem 1rem 1rem;
@@ -125,6 +192,195 @@ function handleClick() {
       position: absolute;
       top: 0;
     }
+  } */
+
+  :root {
+    --aboveLine-clr: hsl(22, 81%, 35%);
+    --additionLaterHand-color: hsla(91, 59%, 64%, 0.3);
+    --belowLine-clr: hsl(22, 81%, 35%);
+    --commentary-color: #e3dbcc99;
+    --commentary-opacity: 0.9;
+    --correction-clr: hsla(36, 85%, 76%, 0.6);
+    --deleted-text-decoration: line-through;
+    --deleted-clr: hsl(0, 0%, 44%);
+    --expansion-clr: hsl(201, 22%, 87%);
+    --expansion-opacity: 0.7;
+    --gap-text-decoration: underline dotted;
+    --head-font-weight: 700;
+    --initial-clr: hsl(114, 100%, 37%);
+    --marginNote-clr: hsl(34, 100%, 47%);
+    --repeated-clr: #cbcbcb;
+    --repeated-text-decoration: line-through;
+    --rubricated-clr: hsl(0, 76%, 38%);
+    --rubricated-font-weight: 700;
+    --sic-clr: hsl(130, 99%, 29%);
+    --transposition-clr: #ccc;
+    --unclear-clr: hsl(328, 98%, 47%);
+  }
+
+  /* -------- TEXT ------------------------------------------------------------------------------------------------------------ */
+
+  span.emphasised.rubricated {
+    font-weight: var(--rubricated-font-weight);
+    color: var(--rubricated-clr);
+  }
+
+  span.emphasised.bold {
+    font-weight: bold;
+    color: unset;
+  }
+
+  span.emphasised.initial {
+    color: var(--initial-clr);
+    font-weight: bold;
+  }
+
+  span.emphasised.italic {
+    font-style: italic;
+  }
+
+  span.emphasised.smallCaps {
+    text-decoration: dotted underline;
+  }
+
+  span.emphasised.underlined {
+    text-decoration: underline;
+  }
+
+  span.emphasised:where(.bold, .initial, .italic, .underlined, .smallCaps) {
+    font-weight: normal;
+    color: unset;
+  }
+
+  span.expansion {
+    background-color: var(--expansion-clr);
+    opacity: var(--expansion-opacity);
+  }
+
+  span.nonLinear.aboveLine {
+    color: var(--aboveLine-clr);
+  }
+
+  span.nonLinear.belowLine {
+    color: var(--belowLine-clr);
+  }
+
+  span.nonLinear.marginNote {
+    color: var(--marginNote-clr);
+  }
+
+  span.correction {
+    background-color: var(--correction-clr);
+  }
+
+  span.deleted.start {
+    border-right: 2px solid var(--deleted-clr);
+  }
+
+  span.deleted.end {
+    border-left: 2px solid var(--deleted-clr);
+  }
+
+  span.unclear {
+    color: var(--unclear-clr);
+  }
+
+  span.repeated {
+    background-color: var(--repeated-clr);
+    text-decoration: var(--repeated-text-decoration);
+  }
+
+  span.gap {
+    text-decoration: var(--gap-text-decoration);
+  }
+
+  span.additionLaterHand {
+    background-color: var(--additionLaterHand-color);
+  }
+
+  span.head {
+    font-weight: var(--head-font-weight);
+  }
+
+  span.commentary {
+    background-color: var(--commentary-color);
+    opacity: var(--commentary-opacity);
+  }
+
+  span.line.start::before {
+    /* Disable stylings from span element... */
+    background-color: white;
+    color: black;
+    font-weight: normal;
+    opacity: 1;
+    /* Curved arrow */
+    content: '\2937';
+    white-space: pre;
+    user-select: none;
+  }
+
+  span.line.end::after {
+    /* Disable stylings from span element... */
+    background-color: white;
+    color: black;
+    font-weight: normal;
+    opacity: 1;
+    /* Curved arrow, newline */
+    content: '\2936 \A';
+    white-space: pre;
+    user-select: none;
+  }
+
+  span.person {
+    text-decoration: underline;
+    text-decoration-color: hsl(358, 83%, 68%);
+    text-decoration-thickness: 2px;
+  }
+
+  span.place {
+    text-decoration: underline;
+    text-decoration-color: hsl(212, 93%, 54%);
+    text-decoration-thickness: 2px;
+  }
+
+  span.concept {
+    text-decoration: underline;
+    text-decoration-color: hsl(45, 90%, 60%);
+    text-decoration-thickness: 2px;
+  }
+
+  span.event {
+    text-decoration: underline;
+    text-decoration-color: hsl(134, 61%, 41%);
+    text-decoration-thickness: 2px;
+  }
+
+  span.imagery {
+    text-decoration: underline;
+    text-decoration-color: hsl(296, 72%, 66%);
+    text-decoration-thickness: 2px;
+  }
+
+  span.selfReference {
+    text-decoration: underline;
+    text-decoration-color: hsl(55, 78%, 44%);
+    text-decoration-thickness: 2px;
+  }
+
+  span.biblePassage {
+    text-decoration: underline;
+    text-decoration-color: hsl(37, 61%, 51%);
+    text-decoration-thickness: 2px;
+  }
+
+  span.literatureReference {
+    text-decoration: underline;
+    text-decoration-color: hsl(32, 92%, 47%);
+    text-decoration-thickness: 2px;
+  }
+
+  span.sic {
+    color: var(--sic-clr);
   }
 }
 </style>
